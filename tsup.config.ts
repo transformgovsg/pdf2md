6857de88d508c33d886a074cdb24a62d06645fa4
YimingIsCOLD <yimingpeh@gmail.com>
2025-03-09 04:01:33 +0800

    chore: add `prettier` for code formatting

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
-  format: "esm",
+  entry: ['./src/index.ts'],
+  format: 'esm',
   dts: true,
   minify: true,
   sourcemap: true,
